276ba6097a2f52340ce42ab868d4a1e11f01b726
Tim <tim@timrcm.com>
2018-06-14 13:33:39 -0600

    Created function for Cisco devices

--- blankconfig.py
+++ blankconfig.py
@@ -1,4 +1,10 @@
-# Set the host, user, and password 
-host = ''
-user = ''
-pw = ''
+# Set the host, user, and passwords for each device
+# Add more devices as needed
+
+CISCO_DEVICES = [
+    ('123.123.123.123', 'username', 'password'),
+    ('123.123.123.124', 'username', 'password'),
+    ('123.123.123.125', 'username', 'password'),
+    ('123.123.123.126', 'username', 'password'),
+    ('123.123.123.127', 'username', 'password')
+    ]
--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 # STOP! Fill out blankconfig.py and rename it to config.py before running.
 
 import paramiko 
-import time
+from time import sleep
 
 import config
 
 ssh = paramiko.SSHClient() 
-
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(config.host, username=config.user, password=config.pw)
 
-ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('''
-ls -a
-echo " "
-uptime
-echo " "
-ls -a /var/www
-''')
+def cisco_backup(cmd): 
+    for X in config.CISCO_DEVICES:
+        ssh.connect(X[0], username=X[1], password=X[2])
+        cmdtype = cmd.replace(' ', '.')
+        path = f'{X[0]}.{cmdtype}.txt'
+        backup_file = open(path, 'w+', newline='\n')
 
-ssh_stdout = ssh_stdout.readlines()
-time.sleep(1)
-ssh.close()
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd)
 
-for line in ssh_stdout:
-    print(line, end='')
+        ssh_stdout = ssh_stdout.readlines()
+
+        sleep(1)
+        ssh.close()
+
+        for line in ssh_stdout:
+            backup_file.write(line)
+
+        print(f"{path} created.")
+
+def main():
+    print("Starting backups...")
+    cisco_backup('show run')
+    cisco_backup('show ver')
+    cisco_backup('show flash')
+    print("All devices found in config completed.")
+    exit(0)
+
+main()
